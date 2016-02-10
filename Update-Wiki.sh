69d122fc02817174db5e713c6d51c0a154afaf31
Rafael J. Rodriguez <rafase282@gmail.com>
2016-02-10 15:40:26 -0500

    Added current script

---
+++ Update-Wiki.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Setup paths
+LOCALWIKI="C:\Users\rafas\Documents\wiki"
+LIVEWIKI="C:\Users\rafas\Documents\FreeCodeCamp.wiki"
+
+echo "FreeCodeCamp Wiki updater v1"
+echo
+echo "Move to Local Wiki directory at " + $LOCALWIKI
+cd $LOCALWIKI
+echo
+echo "Updating local repository..."
+git pull
+git status
+echo
+echo "Done!"
+echo "Going to the live Wiki ..."
+cd $LIVEWIKI
+echo
+echo "Update live wiki..."
+git fetch upstream
+git pull upstream master
+git push
+echo "Done!"
+echo
+# Keep the windows Open until I press a key
+echo "Press any key to close this windows..."
+read -n 1 -s
