ac4d72c770776a0c6050f4373db8cc7d41015d5c
Rafael J. Rodriguez <rafase282@gmail.com>
2016-02-12 23:01:12 -0500

    Added simple linux version, no conf file needed

---
+++ simple-update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Setup paths for local and live wiki. Upstream must be setup prior to run this script.
+LOCALWIKI="/home/pi/workspace/wiki"
+LIVEWIKI="/home/pi/workspace/FreeCodeCamp.wiki"
+
+echo "FreeCodeCamp Wiki updater v1"
+echo
+date
+echo
+echo "Move to Local Wiki directory at " + $LOCALWIKI
+cd $LOCALWIKI
+echo
+echo "Updating local repository..."
+git pull
+git status
+echo
+echo "Done!"
+echo "Going to the live Wiki ..."
+cd $LIVEWIKI
+echo
+echo "Update live wiki..."
+git fetch upstream
+git pull upstream master
+git push
+echo "Done!"
+cd /home/pi/
+echo
+echo
